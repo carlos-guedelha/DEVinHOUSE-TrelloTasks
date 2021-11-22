a13991fbc2912f2b61a0d4ce3802940adcfc0a44
jrguedelha <jrguedelha@gmail.com>
2021-11-22 11:49:15 -0300

    M1S02-Ex01

---
+++ M1S02/Ex1/scripts.js
@@ -0,0 +1,11 @@
+var nome = window.prompt("Qual o seu nome?");
+var idade = window.prompt("Informe sua idade");
+var esportePredileto = window.prompt("Qual seu esporte favorito?");
+var diaDaSemana = window.prompt("Qual dia da semana você prfere?");
+var alguemImportante = window.prompt("Me diga o nome de algúem importante");
+var alguemAdmiravel = window.prompt("Informe o nome de algúem que você admira");
+
+window.alert(`"Hoje, ${diaDaSemana}, é um dia histórico. ` +
+`Todos os fãs de ${esportePredileto} estão maravilhados com a proeza realizada pelo(a) atleta ${nome}. ` + 
+`${alguemImportante} não conteve as lágrimas ao comentar sobre em uma entrevista ` + 
+`hghhe até o(a) ${alguemAdmiravel} twittou “é muito bom ver alguém com ${idade} demonstrando tanta habilidade".`);
